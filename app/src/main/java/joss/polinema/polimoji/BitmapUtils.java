9cff73221c6cbabfeed3027615bbd1704b3fa0a6
rezaLUL <rezaulull@gmail.com>
2018-10-19 15:53:03 +0700

    polimoji

---
+++ app/src/main/java/joss/polinema/polimoji/BitmapUtils.java
@@ -0,0 +1,133 @@
+package joss.polinema.polimoji;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class BitmapUtils {
+
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.panjibs.polimojii.fileprovider";
+    private static final String FOLDER = "polimojii";
+
+    static Bitmap resamplePic(Context context, String imagePath) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager manager = (WindowManager)
+                context.getSystemService(Context.WINDOW_SERVICE);
+        manager.getDefaultDisplay().getMetrics(metrics);
+
+        int targetH = metrics.heightPixels;
+        int targetW = metrics.widthPixels;
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        return BitmapFactory.decodeFile(imagePath);
+    }
+
+    static File createTempImageFile(Context context) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalCacheDir();
+
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+    }
+
+    static boolean deleteImageFile(Context context, String imagePath) {
+        // Get the file
+        File imageFile = new File(imagePath);
+
+        // Delete the image
+        boolean deleted = imageFile.delete();
+
+        // If there is an error deleting the file, show a Toast
+//        if (!deleted) {
+//            String errorMessage = context.getString(Integer.parseInt(context.getString(deleted)));
+//            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
+//        }
+
+        return deleted;
+    }
+
+    private static void galleryAddPic(Context context, String imagePath) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(imagePath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        context.sendBroadcast(mediaScanIntent);
+    }
+
+    static String saveImage(Context context, Bitmap image) {
+
+        String savedImagePath = null;
+
+        // Create the new file in the external storage
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".jpg";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                        + "/" + FOLDER);
+        boolean success = true;
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs();
+        }
+
+        if (success) {
+            File imageFile = new File(storageDir, imageFileName);
+            savedImagePath = imageFile.getAbsolutePath();
+
+
+            try {
+                OutputStream fOut = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+                fOut.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            galleryAddPic(context, savedImagePath);
+            String savedMessage = "saved_message";
+            Toast.makeText(context, savedMessage, Toast.LENGTH_SHORT).show();
+        }
+        return savedImagePath;
+    }
+
+    static void shareImage(Context context, String imagePath)
+    {
+        // Create the share intent and start the share activity
+        File imageFile = new File(imagePath);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        Log.d("Share Images", "shareImage: "+imageFile);
+        shareIntent.setType("image/*");
+        Uri photoURI = FileProvider.getUriForFile(context, "joss.polinema.fileprovider", imageFile);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
+        context.startActivity(shareIntent);
+    }
+
+}
